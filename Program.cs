506e3cc11508e6c69c530aa4a528aa38d888ba91
0528sg <65436160+0528sg@users.noreply.github.com>
2020-05-17 13:57:23 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace acmicpc2941
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] croatia= { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+            // change croatia alphabets to one alphabet.
+            for (int i = 0; i < croatia.Length; i++)
+            {
+                input = input.Replace(croatia[i], "a");
+            }
+
+            Console.WriteLine(input.Length);
+        }
+    }
+}
